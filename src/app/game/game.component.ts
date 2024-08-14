c75888bb81402aa46a27831473382cd5e3eeba76
wavyStructures <anja_schwab@gmx.de>
2024-08-14 09:56:57 +0200

    show picked card remove from game.stack

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Game } from "../models/game";
 
 @Component({
   selector: 'app-game',
@@ -8,10 +9,33 @@ import { CommonModule } from '@angular/common';
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
   pickCardAnimation = false;
+  currentCard: string | undefined = '';
+  game!: Game;  // Use the definite assignment assertion
+
+  constructor() {}
+
+  ngOnInit(): void {
+      this.newGame();
+  }
+
+
+  newGame(){
+    this.game = new Game();
+    console.log(this.game);
+  }
+
 
   takeCard() {
+    if (!this.pickCardAnimation)
+    this.currentCard = this.game.stack.pop();
+    console.log(this.currentCard);
     this.pickCardAnimation = true;
+
+    setTimeout(() => {
+      this.pickCardAnimation = false;
+    }, 1500);
   }
+
 }
