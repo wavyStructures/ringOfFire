c9eca43cbfa70c4c48f9015982c9d0a69324091a
wavyStructures <anja_schwab@gmx.de>
2024-08-14 08:13:23 +0200

    top card pick-card-animation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
+  pickCardAnimation = false;
 
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
 }
