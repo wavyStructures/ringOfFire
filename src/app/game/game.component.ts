09a7eca95c29878bf9ab54e3b995e150a2add9d4
wavyStructures <anja_schwab@gmx.de>
2024-08-17 08:16:23 +0200

    first playable version

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -57,7 +57,7 @@ export class GameComponent implements OnInit{
     const dialogRef = this.dialog.open(DialogAddPlayerComponent);
 
     dialogRef.afterClosed().subscribe((name: string) => {
-      if (name.length > 0){
+      if (name && name.length > 0){
       this.game.players.push(name);}
       });
   }
